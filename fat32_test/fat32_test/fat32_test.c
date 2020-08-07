915929911f14b99a329d408dbd1a91a23951a944
Bessie <33269462+BessieChen@users.noreply.github.com>
2020-08-08 07:01:16 +0800

    create FAT32_FileSystem

---
+++ fat32_test/fat32_test/fat32_test.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+static int array[32];
+
+//打印整型变量的函数
+void print_int(int v)
+{
+	printf("v=%d\n", v);
+}
+int main(void)
+{
+	int* p_array = array;
+
+	for (int i = 0; i < 100; i++)
+	{
+		print_int(i);
+	}
+
+	p_array[0] = 32;
+
+	printf("Test End");
+	return 0;
+}
